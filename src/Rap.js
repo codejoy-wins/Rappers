ff4a7c3ded682d0e26947a8671eb9f9d5a6e82ac
Max <mjann@ucsc.edu>
2019-07-14 16:04:01 -0700

    so bad

---
+++ src/Rap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Rap(props){
+    return (
+        <div>
+            <h1>{props.magic.name}</h1>
+            <p>{props.magic.desc}</p>
+        </div>
+    )
+}
+
+export default Rap;
---
+++ src/falseData.js
@@ -0,0 +1,28 @@
+const falseData = [
+    {
+        id: 1,
+        name: "Eminem",
+        desc: "I love Eminem's rhymes",
+        song: "So Bad"
+    },
+    {
+        id: 2,
+        name: "Asap Rocky",
+        desc: "I love Asap Rocky's beats",
+        song: "Praise the Lord"
+    },
+    {
+        id: 3,
+        name: "Logic",
+        desc: "I love Logic's new stuff",
+        song: "Homicide"
+    },
+    {
+        id: 4,
+        name: "Mac Miller",
+        desc: "Rest in peace",
+        song: "Self Care"
+    },
+]
+
+export default falseData;
